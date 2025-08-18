5b32c3cad5049eeeff68466787b4b3651f6e3ffe
tialiancadivergente <ti@aliancadivergente.com.br>
2025-08-18 18:35:08 -0300

    feat: adicionar novas páginas e componentes para o diagnóstico de dependência emocional

---
+++ app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import SplashScreen from "@/app/components/SplashScreen"
+import Formv10 from "@/app/ordo/[version]/v10"
+
+
+export default function Home() {
+
+  return (
+    <SplashScreen>
+      <Formv10 />
+    </SplashScreen>
+  )
+} 
