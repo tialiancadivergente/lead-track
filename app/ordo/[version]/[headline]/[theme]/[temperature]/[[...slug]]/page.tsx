92b8aae057b8c09a9e308285873fe364fb3ad6dd
tialiancadivergente <ti@aliancadivergente.com.br>
2025-08-21 00:56:29 -0300

    feat: adicionar suporte à versão 20 do formulário, incluindo lógica de exibição e novos componentes visuais

--- app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
+++ app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -3,6 +3,7 @@
 import SplashScreen from "@/app/components/SplashScreen"
 import Formv10 from "@/app/ordo/[version]/v10"
 import Formv19 from "@/app/ordo/[version]/v19"
+import Formv20 from "@/app/ordo/[version]/v20"
 import { useParams } from "next/navigation"
 
 
@@ -14,6 +15,15 @@ export default function Home() {
     return (
         <SplashScreen>
           <Formv19 />
+        </SplashScreen>
+    )
+  }
+
+  if (version === 'v20') {
+    
+    return (
+        <SplashScreen>
+          <Formv20 />
         </SplashScreen>
     )
   }
