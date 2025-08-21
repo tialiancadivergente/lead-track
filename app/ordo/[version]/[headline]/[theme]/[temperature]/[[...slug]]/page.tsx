ea057da1d129f8575deb8dc0e5cf64311adec0f0
tialiancadivergente <ti@aliancadivergente.com.br>
2025-08-21 08:13:59 -0300

    refactor: padronizar formatação de código e melhorar a legibilidade no componente de página

--- app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
+++ app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -1,36 +1,33 @@
-"use client"
+"use client";
 
-import SplashScreen from "@/app/components/SplashScreen"
-import Formv10 from "@/app/ordo/[version]/v10"
-import Formv19 from "@/app/ordo/[version]/v19"
-import Formv20 from "@/app/ordo/[version]/v20"
-import { useParams } from "next/navigation"
-
+import SplashScreen from "@/app/components/SplashScreen";
+import Formv10 from "@/app/ordo/[version]/v10";
+import Formv19 from "@/app/ordo/[version]/v19";
+import Formv20 from "@/app/ordo/[version]/v20";
+import { useParams } from "next/navigation";
 
 export default function Home() {
-  const { version } = useParams()
+  const { version } = useParams();
 
-  if (version === 'v19') {
-    
+  if (version === "v19") {
     return (
-        <SplashScreen>
-          <Formv19 />
-        </SplashScreen>
-    )
+      <SplashScreen>
+        <Formv19 />
+      </SplashScreen>
+    );
   }
 
-  if (version === 'v20') {
-    
+  if (version === "v20") {
     return (
-        <SplashScreen>
-          <Formv20 />
-        </SplashScreen>
-    )
+      <SplashScreen>
+        <Formv20 />
+      </SplashScreen>
+    );
   }
 
   return (
     <SplashScreen>
       <Formv10 />
     </SplashScreen>
-  )
-} 
+  );
+}
