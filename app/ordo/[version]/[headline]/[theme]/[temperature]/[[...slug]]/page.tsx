781a2dcb81328d2da75118ac1007f964b538abf3
tialiancadivergente <ti@aliancadivergente.com.br>
2025-08-27 11:09:47 -0300

    feat: adicionar suporte à versão 8 do formulário com novos componentes visuais, lógica de captura de UTMs e melhorias na experiência do usuário

--- app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
+++ app/ordo/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -7,10 +7,25 @@ import Formv16 from "@/app/ordo/[version]/v16";
 import Formv19 from "@/app/ordo/[version]/v19";
 import Formv20 from "@/app/ordo/[version]/v20";
 import Formv11 from "@/app/ordo/[version]/v11";
+import Formv8 from "@/app/ordo/[version]/v8/indext";
+import JourneySection from "@/components/journey-section"
+import MentorSection from "@/components/mentor-section"
+import Footer from "@/components/footer"
 import { useParams } from "next/navigation";
 
 export default function Home() {
   const { version } = useParams();
+
+  if (version === "v8") {
+    return (
+      <SplashScreen>
+        <Formv8 />
+        <JourneySection />
+        <MentorSection />
+        <Footer />
+      </SplashScreen>
+    );
+  }
 
   if (version === "v11") {
     return (
