9467dc0d629eafa8f72a67c873ac2183cf359644
jacqueline spineli <pp.spineli@gmail.com>
2026-03-01 17:45:20 -0300

    Criação da v3 e v4 oro

--- app/oro/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
+++ app/oro/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -4,9 +4,27 @@ import { useParams } from "next/navigation";
 import Formv1 from "@/app/oro/[version]/v1";
 import Formv2 from "@/app/oro/[version]/v2";
 import SplashScreenOro from "@/app/components/SplashScreen/SplashScreenOro";
+import Formv3 from "@/app/oro/[version]/v3";
+import Formv4 from "@/app/oro/[version]/v4";
+
 
 export default function Home() {
   const { version } = useParams();
+
+ if (version === "v4") {
+    return (
+      <SplashScreenOro>
+        <Formv4 />
+      </SplashScreenOro>
+    )
+  }
+  if (version === "v3") {
+    return (
+      <SplashScreenOro>
+        <Formv3 />
+      </SplashScreenOro>
+    )
+  }
 
   if (version === "v2") {
     return (
