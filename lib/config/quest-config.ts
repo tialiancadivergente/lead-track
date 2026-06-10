f8c60e8642b2866983220358c919858ab1623778
tialiancadivergente <ti@aliancadivergente.com.br>
2026-06-10 10:28:35 -0300

    Update default quest form version ID in quest-config.ts to reflect the latest version.

--- lib/config/quest-config.ts
+++ lib/config/quest-config.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_QUEST_FORM_VERSION_ID =
-  "2f76bc57-57a2-41fd-9c2c-18a726dd4fe0";
+  "c0909c2f-4bb6-49fe-8fac-7153076f6781";
 
 const QUEST_WHATSAPP_URL_BY_TEMPERATURE = {
   f: "https://sendflow.click/i/oro-jun26",
